01a9296dff908416c10504f317082d54baef19a8
viktorIlyin <113107257+viktorIlyin@users.noreply.github.com>
2023-07-25 10:50:38 +0400

    start adding allure reports

--- src/test/java/RegistrationPageTest.java
+++ src/test/java/RegistrationPageTest.java
@@ -3,6 +3,8 @@ import Pages.LkPage;
 import Pages.RegistrationPage;
 import com.codeborne.selenide.Selenide;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.*;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -27,6 +29,8 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Успешная регистрация")
+    @Description("Успешная регистрация с корректными данными")
     public void testSuccessfulRegistration() {
         registrationPage.registration(userGenerator.login, userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , userGenerator.phoneNumber);
         String actualResult = registrationPage.getErrorAuthorizationMessage();
@@ -34,6 +38,8 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Регистрация с уже зарегистрированным логином")
+    @Description("Регистрация с уже зарегистрированным логином, но остальные данные корректны")
     public void testRegistrationWithExistingLogin() {
         registrationPage.registration(EXISTED_LOGIN,  userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , userGenerator.phoneNumber);
         String actualResult = registrationPage.getErrorAuthorizationMessage();
@@ -41,6 +47,7 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Регистрация с уже зарегистрированным Email")
     public void testRegistrationWithExistingEmail() {
         registrationPage.registration(userGenerator.login, userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, EXISTED_EMAIL, userGenerator.phoneNumber);
         String actualResult = registrationPage.getErrorAuthorizationMessage();
@@ -48,6 +55,7 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Регистрация с уже зарегистрированным номером телефона")
     public void testRegistrationWithExistingPhone() {
         registrationPage.registration(userGenerator.login ,userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , EXISTED_PHONE);
         String actualResult = registrationPage.getErrorAuthorizationMessage();
@@ -55,6 +63,7 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Регистрация с некорректным повторением пароля")
     public void testInvalidRepeatPasswords() {
         registrationPage.registration(userGenerator.login, userGenerator.password, " ", userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , userGenerator.phoneNumber);
         String actualResult = registrationPage.getErrorPasswordMessage();
@@ -62,6 +71,7 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Регистрация с пустым полем номера телефона")
     public void testMissingPhoneField() {
         registrationPage.registration(userGenerator.login, userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , " ");
         String actualResult = registrationPage.getErrorPhoneMessage();
@@ -69,24 +79,28 @@ public class RegistrationPageTest {
     }
 
     @Test
+    @DisplayName("Регистрация с пустым полем пароля")
     public void testMissingPasswordFields() {
         registrationPage.registration(userGenerator.login, " ", userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , userGenerator.phoneNumber);
         String actualResult = registrationPage.getErrorPasswordMessage();
         Assert.assertEquals("Текст не совпадает", "Пароли не совпадают", actualResult);
     }
     @Test
+    @DisplayName("Регистрация без установленной галочки подписки на Email сообщения")
     public void testMissingCheckBoxEmail() {
         registrationPage.clickEmailRegistrationCheckBox();
         registrationPage.registration(userGenerator.login, userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , userGenerator.phoneNumber);
     //Добавить ассерт какой-нибудь на то, что тест работает корректно.
     }
     @Test
+    @DisplayName("Регистрация без установленной галочки на пользовательское соглашение")
     public void testMissingCheckBoxAgreement() {
         registrationPage.clickPersonalAgreementCheckBox();
         registrationPage.registration(userGenerator.login, userGenerator.password, userGenerator.password, userGenerator.lastName, userGenerator.firstName, userGenerator.middleName, userGenerator.email , userGenerator.phoneNumber);
         //Добавить ассерт какой-нибудь на то, что тест работает корректно.
     }
     @Test
+    @DisplayName("Кнопка глаза показывает пароль и возможно получить пароль через getValue")
     public void testPasswordVisabilityRegistrationTest(){
         registrationPage.setPasswordField("123456");
         registrationPage.clickEyeVisibleInvisible();
@@ -103,8 +117,6 @@ public class RegistrationPageTest {
         // Пример: Selenide.screenshot("screenshot_name");
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
-        // Пример: Selenide.clearBrowserCookies();
-        // Пример: Selenide.closeWebDriver();
         Selenide.closeWebDriver();
     }
 }
